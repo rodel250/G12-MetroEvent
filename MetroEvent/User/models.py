92ec11fc3c29a73008d8723efff5230b8b52d1eb
rodel250 <69116852+rodel250@users.noreply.github.com>
2021-03-21 17:13:23 +0800

    test

--- MetroEvent/User/models.py
+++ MetroEvent/User/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length = 50, null=False, blank=False,primary_key = True,default = 'default')
     firstname = models.CharField(max_length = 50, null=True, blank=True)
+    middlename = models.CharField(max_length = 50, null=True, blank=True)
     lastname = models.CharField(max_length = 50, null=True, blank=True)
     password = models.CharField(max_length = 50, null=True, blank=True)
     emailAddress = models.CharField(max_length = 50, null=True, blank=True)
+    gender = models.CharField(max_length = 50, null=True, blank=True)
     birthdate = models.DateField()
     class Meta:
         db_table = "User"
